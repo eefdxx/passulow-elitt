51455b0f7f0c4db16256ca99f29c18920e375e42
eefdxx <muhadfarid@gmail.com>
2023-06-03 13:48:20 +0700

    Tambah geleri foto

--- js/script.js
+++ js/script.js
@@ -9,6 +9,24 @@ document.querySelector("#hamburger-menu").onclick = () => {
 // klik diluar sidebar untuk menghilangkan nav
 const hamburger = document.querySelector("#hamburger-menu");
 
+// Slideshow
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+
 document.addEventListener("click", function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
